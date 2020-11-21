67f4c942ec506313ce63cc619fbb06c28f8062b2
Abby <abigaild2424@gmail.com>
2020-11-21 01:15:12 -0500

    found a better calendar template. Replaced old component and implemeted new calendar component.  Will need to customize to fit my needs

--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VCalendar from 'v-calendar';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
-Vue.use(VCalendar, {
-  title: 'MMMM YYYY',
-  weekdays: 'W',
-  navMonths: 'MMM',
-  input: ['L', 'YYYY-MM-DD', 'YYYY/MM/DD'],
-  dayPopover: 'WWW, MMM D, YYYY',
-  data: ['L', 'YYYY-MM-DD', 'YYYY/MM/DD'],
-})
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
